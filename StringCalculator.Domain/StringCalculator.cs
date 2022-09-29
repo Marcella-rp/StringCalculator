5c86c0a102cec5f90469c2949cb9ae3df9b6b39c
Marcella R Pastore <marcella.rpastore@gmail.com>
2022-09-28 21:56:18 -0300

    Adicionar arquivos de projeto.

---
+++ StringCalculator.Domain/StringCalculator.cs
@@ -0,0 +1,26 @@
+﻿namespace StringCalculator.Domain
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if(string.IsNullOrEmpty(numbers))
+                return 0;
+
+            var parts = numbers.Split(',');
+
+            var exceedsCount = parts.Length > 2;
+            var consecutiveComands = parts.Any(x => x == "");
+            var nonNumbers = parts.Any(x => !int.TryParse(x, out _));
+
+
+            if (exceedsCount || consecutiveComands || nonNumbers)
+                throw new ArgumentException("O parametro deve conter no maximo 2 numeros", nameof(numbers));
+
+            // var sum = parts.Sum(Convert.ToInt32);
+            var sum = parts.Sum(x=> Convert.ToInt32(x));
+
+            return sum;
+        }
+    }
+}
